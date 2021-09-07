184cf072d76c259c8cc2d4b83b7221984bea775b
mohammedalsamki <momosamak21@gmail.com>
2021-09-07 03:46:36 -0400

    lab 2

---
+++ src/component/BestSelect.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react'
+
+import { Modal, Button, Card } from 'react-bootstrap'
+
+class BestSelect extends Component {
+    render() {
+        return (
+            <>
+
+                <Modal Show={this.props.dataShow}>
+                    <Modal.Header>
+                        <Modal.Title>{this.props.datamodel}</Modal.Title>
+                        </Modal.Header>
+                        <Modal.Body>
+
+
+                            <Card>
+                                <Card.Img src={this.props.datamodel.img_url} alt={this.props.title} />
+                                <Card.Body>
+                                    <Card.Text>
+                                        {this.props.description}
+                                    </Card.Text>
+                                </Card.Body>
+                            </Card>
+                        </Modal.Body>
+                        <Modal.Footer>
+                            <Button onClick={this.props.exitHandler}>Close</Button>
+                        </Modal.Footer>
+                        </Modal>
+
+
+
+
+                    </>
+                    )
+
+
+
+
+    }
+}
+                    export default BestSelect
--- src/component/Main.js
+++ src/component/Main.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react'
+import HornbeastData from "./data.json"
+import HornBeast from './HornBeast'
 
-export class Main extends Component {
+
+class Main extends Component {
     render() {
         return (
             < div style={{
@@ -13,13 +16,25 @@ export class Main extends Component {
                 alignItems: "center",
                 backgroundColor: "gray"
             }}>
-                <h2>{this.props.title}</h2>
+                {/* <h2>{this.props.title}</h2>
                 <img alt={this.props.keyword} style={{ width: "600px" }} src={this.props.image_url} />;
 
                 <p>Number Of Horns: {this.props.horns}</p>
 
                 <p>{this.props.description}</p>
+                
+                */}{
+                    HornbeastData.map(elem =>{
+                    return(
+                        <HornBeast 
+                        title={elem.title}
+                        img_url={elem.image_url}
+                        description={elem.description}
 
+                        />
+                    )
+                })
+                }
 
             </div>
         )
